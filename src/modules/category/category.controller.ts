4fdd183f20c4f896f0bd7732681b5c625366738f
thaibh <thaibh@etc.vn>
2024-04-02 16:14:23 +0700

    crud category

--- src/modules/category/category.controller.ts
+++ src/modules/category/category.controller.ts
@@ -1,4 +1,49 @@
-import { Controller } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  UseGuards,
+} from "@nestjs/common";
+import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
+import { AuthGuard } from "src/guards/auth.guard";
+import { CategoryService } from "./category.service";
+import { CreateCategoryDto } from "./dtos/create-category.dts";
 
-@Controller()
-export class CategoryController {}
+@Controller("category")
+@ApiTags("category")
+export class CategoryController {
+  constructor(private readonly categoryService: CategoryService) {}
+
+  @Get()
+  async getAllCategories() {
+    return await this.categoryService.getAllCategories();
+  }
+
+  @Post()
+  @UseGuards(AuthGuard)
+  @ApiBearerAuth()
+  async addCategory(@Body() dto: CreateCategoryDto) {
+    return await this.categoryService.addCategory(dto);
+  }
+
+  @Put(":id")
+  @UseGuards(AuthGuard)
+  @ApiBearerAuth()
+  async updateCategory(
+    @Param("id") id: number,
+    @Body() dto: CreateCategoryDto
+  ) {
+    return await this.categoryService.updateCategory(id, dto);
+  }
+
+  @Delete(":id")
+  @UseGuards(AuthGuard)
+  @ApiBearerAuth()
+  async deleteCategory(@Param("id") id: number) {
+    return await this.categoryService.deleteCategory(id);
+  }
+}
--- src/modules/category/category.service.ts
+++ src/modules/category/category.service.ts
@@ -1,4 +1,64 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, HttpStatus, Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Category } from "src/entities/category.entity";
+import { DataSource, Repository } from "typeorm";
+import { CreateCategoryDto } from "./dtos/create-category.dts";
+import { Product } from "src/entities/product.entity";
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+  constructor(
+    @InjectRepository(Category) private categoryRepo: Repository<Category>,
+    @InjectRepository(Product) private productRepo: Repository<Product>,
+    private dataSource: DataSource
+  ) {}
+
+  async getAllCategories() {
+    const categories = await this.categoryRepo.find();
+    return categories;
+  }
+
+  async addCategory(dto: CreateCategoryDto) {
+    const category = new Category();
+    category.name = dto.name;
+    return await this.categoryRepo.save(category);
+  }
+
+  async updateCategory(id: number, dto: CreateCategoryDto) {
+    const category = await this.categoryRepo.findOne({ where: { id } });
+    if (!category) {
+      throw new BadRequestException("category not found");
+    }
+    category.name = dto.name;
+    return await this.categoryRepo.save(category);
+  }
+
+  async deleteCategory(id: number) {
+    const category = await this.categoryRepo.findOne({
+      where: { id },
+      relations: ["products"],
+    });
+    if (!category) {
+      throw new BadRequestException("Category not found");
+    }
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+    try {
+      const products = category.products;
+      await this.productRepo.save(
+        products.map((product) => ({
+          ...product,
+          category: null,
+          categoryId: null,
+        }))
+      );
+      await this.categoryRepo.delete(id);
+      await queryRunner.commitTransaction();
+      return { status: HttpStatus.OK, message: "Delete success" };
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      return error;
+    }
+  }
+}
